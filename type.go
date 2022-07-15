709c28d9d776244632fe4830ba000720fc1f295f
pigfall <koi_town@outlook.com>
2022-07-15 09:34:23 +0800

    type.go

--- type.go
+++ type.go
@@ -18,3 +18,14 @@ func FromType(tpe *gen.Type) *Type{
 func (this *Type) Name() string{
 	return this.Type.Name
 }
+
+func (this *Type) AllFldsIncludePK()[]*gen.Field{
+	flds := make([]*gen.Field,len(this.Fields)+1)
+	copy(flds,this.Type.Fields)
+	flds[len(flds)-1] = this.Type.ID
+	return flds
+}
+
+func (this *Type) AllFldsExlucdePK()[]*gen.Field{
+	return this.Fields
+}
