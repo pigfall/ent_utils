eb1e1cbd1b51e673b919b3da9f6daf314766586d
pigfall <koi_town@outlook.com>
2023-03-05 23:52:51 +0800

    up

--- type.go
+++ type.go
@@ -1,31 +1,30 @@
 package ent_utils
 
-import(
+import (
 	"entgo.io/ent/entc/gen"
 )
 
-
-type Type struct{
+type Type struct {
 	*gen.Type
 }
 
-func FromType(tpe *gen.Type) *Type{
+func FromType(tpe *gen.Type) *Type {
 	return &Type{
-		Type:tpe,
+		Type: tpe,
 	}
 }
 
-func (this *Type) Name() string{
+func (this *Type) Name() string {
 	return this.Type.Name
 }
 
-func (this *Type) AllFldsIncludePK()[]*gen.Field{
-	flds := make([]*gen.Field,len(this.Fields)+1)
-	copy(flds,this.Type.Fields)
-	flds[len(flds)-1] = this.Type.ID
+func (this *Type) AllFldsIncludePK() []*gen.Field {
+	flds := make([]*gen.Field, len(this.Fields)+1)
+	flds[0] = this.Type.ID
+	copy(flds[1:], this.Type.Fields)
 	return flds
 }
 
-func (this *Type) AllFldsExlucdePK()[]*gen.Field{
+func (this *Type) AllFldsExlucdePK() []*gen.Field {
 	return this.Fields
 }
